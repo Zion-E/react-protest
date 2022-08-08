bad859a188599625a29c60e687bf7cb276fd68b5
Zion-E <81622869+Zion-E@users.noreply.github.com>
2022-08-09 00:52:27 +0100

    Files uploads

---
+++ components/Background/Background.styles.js
@@ -0,0 +1,149 @@
+import styled from 'styled-components';
+import CrowdLOGO from '../../images/crowd-image.jpg';
+
+
+export const Wrapper = styled.div`
+    padding: 0;
+    height: 600px;
+    
+    display: flexbox;
+    align-items: center;
+
+    .Behind {
+        height: 600px;
+        width: 100%;
+    }
+    
+`;
+
+export const Content = styled.div`
+
+    .Cover {
+        height: 600px;
+        background-color: black;
+        opacity: 0.5;
+    }
+
+    ImageLOGO {
+        margin-top: 5px;
+    }
+
+    height: 400px;
+    width: 100%;
+
+    .Nav {
+        display: flex;
+        justify-content: center;
+        margin-top: 2%;
+    }
+
+    li {
+        display: inline-block;
+        padding-right: 10px;
+    }
+
+    .SSK {
+        margin-top: 8%;
+
+        @media screen and (max-width: 600px) {
+            font-weight:700; font-size: 50px;
+        }
+        
+        @media screen and (max-width: 400px) {
+            font-weight:500; font-size: 30px;
+        }
+    
+        @media screen and (max-width: 200px) {
+            font-weight:300; font-size: 15px;
+        }
+    }
+    .SOR1 {
+        color: transparent;
+        -webkit-text-stroke-color: white;
+        -webkit-text-stroke-width: 2px;
+        font-family: Roboto; font-style:normal;
+        font-weight:900; font-size: 70px;
+        margin-top: 30px;
+    }
+    .SOR2 {
+        color: white;
+        font-family: Roboto; margin-top: -48px;
+        font-weight:900; 
+        font-style:normal; 
+        font-size: 70px;
+    }
+    .SOR3 {
+        color: transparent;
+        -webkit-text-stroke-color: white;
+        -webkit-text-stroke-width: 2px;
+        font-family: Roboto; font-style:normal;
+        font-weight:900; font-size: 70px;
+        margin-top: -47px;
+    }
+
+    .Donate {
+        width: 120px; border-radius: 40px; height: 40px; margin-top: 50px;
+        background-color: green; margin-left: 45%; color: white;
+            }
+    
+
+`;
+
+export const Text = styled.div`
+
+    align-text: flex-center;
+
+    color: transparent;
+    -webkit-text-stroke-color: white;
+    -webkit-text-stroke-width: 2px;
+    font-family: Roboto; font-style:normal;
+    font-weight:900; font-size: 70px;
+
+    @media screen and (max-width: 800px) {
+        font-weight:700; font-size: 60px;
+    }
+    
+    @media screen and (max-width: 500px) {
+        font-weight:500; font-size: 40px;
+    }
+
+    @media screen and (max-width: 300px) {
+        font-weight:300; font-size: 20px;
+    }
+    position: center;
+    width: 250px;
+    height: 35px;
+    background: green;
+    margin-top: -30%;
+    margin-left: 25%
+
+`;
+
+export const CrowdImg = styled.img`
+display: flex;
+width: 100%;
+height: 500px;
+
+@media screen and (max-width: 400px) {
+    width: 100%;
+}
+`;
+
+export const ImageLOGO = styled.img`
+
+    display: flex;
+    justify-content: center;
+    margin-left: 45%;
+
+    
+
+
+@media screen and (max-width => 800px) {
+    width: 50%;
+    height: 50px;
+}
+@media screen and (max-width => 400px) {
+    width: 20%;
+}
+`;
+
